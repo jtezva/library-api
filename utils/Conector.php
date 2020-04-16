e548eaaf96c8178b052193c115d9c795ad1a8f88
Emmanuel <emmanuel.hernandezramirez@gmail.com>
2020-04-15 19:18:36 -0500

    Modifiacion de acceso a base de datos

--- utils/Conector.php
+++ utils/Conector.php
@@ -1,7 +1,7 @@
 <?php
 class Conector {
     static function getConection() {
-        $mysqli = new mysqli("localhost", "emmanuel", "12345", "libray");
+        $mysqli = new mysqli("localhost", "emmanuel", "SO76699737427", "library");
         if (mysqli_connect_errno()) {
             throw new Exception("Conection error: " . mysqli_connect_error(), 500);
         }
