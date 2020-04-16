618bd357291c2bb54f74567fb5aadb6365c6fabc
Emmanuel <emmanuel.hernandezramirez@gmail.com>
2020-04-15 19:11:19 -0500

    Conneccion exitosa

--- utils/Conector.php
+++ utils/Conector.php
@@ -1,7 +1,7 @@
 <?php
 class Conector {
     static function getConection() {
-        $mysqli = new mysqli("localhost", "emmanuel", "12345", "libray");
+        $mysqli = new mysqli("3.135.19.156:3306", "emmanuel", "SO76699737427", "library");
         if (mysqli_connect_errno()) {
             throw new Exception("Conection error: " . mysqli_connect_error(), 500);
         }
