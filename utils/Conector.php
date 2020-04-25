d5c1089899c9f683e9827ab86bb0c6ed15b15638
Esfayro <luisesfayro1@gmail.com>
2020-04-25 10:32:44 -0500

    Commit

--- utils/Conector.php
+++ utils/Conector.php
@@ -1,7 +1,7 @@
 <?php
 class Conector {
     static function getConection() {
-        $mysqli = new mysqli("localhost", "emmanuel", "SO76699737427", "library");
+        $mysqli = new mysqli("3.135.19.156", "emmanuel", "SO76699737427", "library");
         if (mysqli_connect_errno()) {
             throw new Exception("Conection error: " . mysqli_connect_error(), 500);
         }
